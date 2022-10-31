29521c1417f359dbf56915dce0f7be23f0ce118b
everjay <everjay@everjays-MBP.domain>
2022-10-30 18:02:54 -0700

    add speechrecognition and app.py

---
+++ src/app.py
@@ -0,0 +1,13 @@
+class NeverMore:
+    def __init__(self):
+        pass
+
+    def main(self):
+        """_summary_
+        """
+        print("quoth the raven")
+
+
+if __name__ == "__main__":
+    never_more = NeverMore()
+    never_more.main()
